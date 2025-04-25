e89c046fcb548cd354af348af386f210dfb823fa
victoryone <65767043+wsw579@users.noreply.github.com>
2025-04-26 03:06:18 +0900

    [level 1] Title: 숫자 짝꿍, Time: 439.48 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,22 +1,14 @@
 from collections import Counter
 
 def solution(X, Y):
-    counter_X = Counter(X)
-    counter_Y = Counter(Y)
+    common = Counter(X) & Counter(Y)
     
-    answer = []
-    
-    for digit in counter_X:
-        if digit in counter_Y: 
-            count = min(counter_X[digit] , counter_Y[digit])
-            answer.extend(count * digit)
-            
-    if not answer:
+    if not common:
         return '-1'
-    
+
+    answer = sorted(common.elements(), reverse=True)
+
     if answer[0] == '0':
         return '0'
-    
-    answer.sort(reverse=True)
-    
+
     return ''.join(answer)
