4eb932cd86b3c1680e2d73d0076155c2a0011227
victoryone <65767043+wsw579@users.noreply.github.com>
2025-05-20 20:47:58 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.09 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,7 +1,7 @@
 def solution(sides):
     answer = 0
     # max(sides)가 가장 긴 변 
-    for num in range(max(sides) - min(sides) , max(sides)):
+    for num in range(max(sides) - min(sides)+1, max(sides)+1):
         answer += 1 
         
     #나머지 한 변이 제일 긴 변 
