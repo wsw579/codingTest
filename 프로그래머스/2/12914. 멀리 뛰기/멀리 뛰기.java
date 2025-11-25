23002b5014edf6a391278102a8f36024bc31c51c
victoryone <65767043+wsw579@users.noreply.github.com>
2025-11-26 01:02:12 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        final int MOD = 1234567;
+        long[] dp = new long[n + 1];
+        
+        
+        if(n >= 0){
+            dp[0] =1;
+        }
+        if(n >= 1){
+            dp[1] = 1;
+        }
+        
+        
+       
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+        
+        return dp[n];
+    }
+}
