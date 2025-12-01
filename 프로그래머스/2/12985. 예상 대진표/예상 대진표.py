9ecd08949046564aa7ce917c9ec4d94a8884c6f1
victoryone <65767043+wsw579@users.noreply.github.com>
2025-12-02 01:18:48 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,11 @@
+import math
+def solution(n,a,b):
+    answer = 0
+    
+    while a!=b:
+        a = (a+1)// 2 # 2 # 1 
+        b = (b+1) // 2 # 4 # 2 
+        answer += 1 
+        
+        
+    return answer
